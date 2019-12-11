aa497d3e7aa1c14af6d8ecc3b0c91cf2f9820298
Anthony Hill <hillan1152@gmail.com>
2019-12-11 17:41:15 -0500

    Complete Queries

--- queries.sql
+++ queries.sql
@@ -13,5 +13,18 @@ FROM [Order] AS o
     JOIN Shipper AS s ON o.ShipVia = s.Id
 WHERE O.OrderDate < '2012-08-09';
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-
+SELECT Product.ProductName
+    , OrderDetail.Quantity
+    , OrderDetail.Id
+FROM OrderDetail
+    JOIN Product ON OrderDetail.ProductId = Product.id
+Where OrderDetail.OrderId = 10251
+ORDER BY ProductName;
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT distinct od.OrderId as OrderID
+    , c.CompanyName as CompanyName
+    , e.LastName as CustomerLastName
+FROM [Order] as o
+    Join OrderDetail as od on o.id = od.OrderId
+    Join Customer as c on o.CustomerId = c.id
+    Join Employee as e on o.EmployeeId = e.id;
